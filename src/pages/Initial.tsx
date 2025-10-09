18df8cfd2512b79b0c01663514605627c51e9e4d
Ykaroos <joaoitalo1098@gmail.com>
2025-10-08 21:11:05 -0300

    Configurações do tailwind e começo do desenvolvimento

--- src/pages/Initial.tsx
+++ src/pages/Initial.tsx
@@ -3,20 +3,23 @@ import Background from "../assets/Background.png";
 
 export default function Initial (){
     return(
-        <body className="w-screen h-screen bg-cover bg-no-repeat bg-center"
+        <div className="w-screen h-screen bg-cover bg-no-repeat bg-center"
               style={{ backgroundImage: `url(${Background})` }}>
             <div className="flex bg-black/85">
-                <div className="w-1/2 h-screen p-4 flex items-center justify-center">
-                    <img className="w-[500px] h-[500px] object-cover" src={Logo} alt="Logo" />
+                <div className="w-1/2 h-screen flex items-center justify-end">
+                    <img className="w-[60%] max-w-[600px] h-auto object-contain" src={Logo} alt="Logo" />
                 </div>
-                <div className="w-1/2 h-screen p-4 flex items-center justify-center">
-                    <div className="w-[450px] h-[250px] text-center mr-25">
-                        <p className="text-white text-5xl font-sans">Bem-Vindo à</p>
-                        <p className="font-bold text-6xl font-sans text-[#BA1E22]">MORIMITSU</p>
-                        <button className="w-[100px] h-[50px] bg-[#BA1E22] mt-10 text-white rounded-[15px]">Entrar</button>
+
+                <div className="w-1/2 h-screen flex items-center justify-start">
+                    <div className="w-[450px] h-[250px] text-center">
+                        <p className="text-white sm:text-3xl md:text-4xl lg:text-6xl font-arimo">Bem-Vindo à</p>
+                        <p className="font-bold sm:text-4xl md:text-5xl lg:text-7xl font-arimo text-[#BA1E22]">MORIMITSU</p>
+                        <button className="w-[105px] sm:h-[30px] md:h-[40px] lg:h-[60px] bg-[#BA1E22] mt-10  rounded-[15px] object-contain">
+                            <p className="text-white text-lg font-arimo">Entrar</p>
+                        </button>
                     </div>
                 </div>
             </div>
-        </body>
+        </div>
     );
 }
